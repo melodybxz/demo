e8f8fe24787c9c4d9cb52a2608d88a335f918769
Baoxin Zhang <baoxinzhang@berkeley.edu>
2023-09-16 23:44:23 -0700

    Initial Commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by zbx‘s macbook on 2023/9/16.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
