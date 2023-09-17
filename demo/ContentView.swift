10456cecec1a28ee854098e334b41d1342a1626d
melodybxz <melodybxz@gmail.com>
2023-09-17 00:15:26 -0700

    add Hello

--- demo/ContentView.swift
+++ demo/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            Text("Hello")
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
